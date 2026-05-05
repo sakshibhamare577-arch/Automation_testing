f372746716c1f81dab6e63cb63fb638135bf7f1a
SakshiBhamre <sakshibhamare5777@gmail.com>
2026-05-05 13:06:28 +0530

    add my first automation program

--- src/main/java/at/Selenium_automation/Testloginpage2.java
+++ src/main/java/at/Selenium_automation/Testloginpage2.java
@@ -1,5 +1,11 @@
 package at.Selenium_automation;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Testloginpage2 {
+	public static void main(String args[]) {
+		ChromeDriver cd = new ChromeDriver();
+		cd.get("https://practicetestautomation.com/practice-test-login/");
+	}
 
 }
